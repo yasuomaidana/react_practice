6d7b05877807641120130005a61eaa5e1ac9b7cf
yasuomaidana <yasuomaidana@gmail.com>
2022-06-10 18:33:03 -0500

    changing content by header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.scss';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href='/'>Ristorante ConFusion</Navbar.Brand>
+      </Container>
+     </Navbar>
     </div>
   );
 }
