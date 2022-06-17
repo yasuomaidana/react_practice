67fa0054b1346ce09f91b74a59bc7670c9a8ceec
yasuomaidana <yasuomaidana@gmail.com>
2022-06-17 16:23:45 -0500

    Creating dishes object to load the information

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import './App.scss';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes:DISHES
+    }
+  }
+  render () {
+    return (
     <div>
      <Navbar bg="primary" variant="dark">
       <Container>
         <Navbar.Brand href='/'>Ristorante ConFusion</Navbar.Brand>
       </Container>
      </Navbar>
-     <Menu/>
+     <Menu dishes={this.state.dishes}/>
     </div>
-  );
+  )};
 }
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -9,53 +9,21 @@ class Menu extends Component{
     constructor(props){
         super(props);
         this.state = {
-            dishes: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-               ],
-        };
+            selectedDish: null
+        }
     }
+
     render(){
-        const menu = this.state.dishes.map(dish=>{
+        const menu = this.props.dishes.map(dish=>{
             return (
-                <Col key={dish.id} xs={12} className="mt-4">
+                <Col key={dish.id} xs={12} md={5} className="m1 mt-4">
                     <Card>
                         <Row>
                             <Col xs={12} md={3}>
                                 <Card.Img src={dish.image} alt={dish.name} className="img-fluid rounded-start h-100"/>
                             </Col>
                             <Col xs={12} md={8}>
-                                <Card.Body className='text-start'>
+                                <Card.Body className='text-start ml-5'>
                                     <Card.Title>{dish.name}</Card.Title>
                                     <Card.Text>{dish.description}</Card.Text>
                                 </Card.Body>
