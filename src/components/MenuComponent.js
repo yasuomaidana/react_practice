9972e82e6ccc365ec41e4f71c8d57e3f08866d7c
yasuomaidana <yasuomaidana@gmail.com>
2022-06-17 16:50:32 -0500

    Adding behavior throught props

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -13,22 +13,41 @@ class Menu extends Component{
         }
     }
 
+    onDishSelect(dish){
+        this.setState({selectedDish:dish});
+    }
+
+    renderDish(dish){
+        if(dish!=null){
+            return(
+            <Card>
+                <Card.Img width={100} src={dish.image} alt={dish.name} className="img-fluid rounded-start"/>
+                <Card.Body>
+                    <Card.Title>{dish.name}</Card.Title>
+                    <Card.Text>{dish.description}</Card.Text>
+                </Card.Body>
+            </Card>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
     render(){
         const menu = this.props.dishes.map(dish=>{
             return (
                 <Col key={dish.id} xs={12} md={5} className="m1 mt-4">
-                    <Card>
-                        <Row>
-                            <Col xs={12} md={3}>
-                                <Card.Img src={dish.image} alt={dish.name} className="img-fluid rounded-start h-100"/>
-                            </Col>
-                            <Col xs={12} md={8}>
-                                <Card.Body className='text-start ml-5'>
-                                    <Card.Title>{dish.name}</Card.Title>
-                                    <Card.Text>{dish.description}</Card.Text>
-                                </Card.Body>
-                            </Col>
-                        </Row>
+                    <Card onClick={()=> this.onDishSelect(dish)}>
+                        <Card.Img width={100} src={dish.image} alt={dish.name} className="img-fluid rounded-start"/>
+
+                        <Card.ImgOverlay className='text-start ml-5'>
+                            <Card.Title><p className='h1'>{dish.name}</p></Card.Title>
+                            
+                        </Card.ImgOverlay>
+                            
                     </Card>
                 </Col>
             )
@@ -37,6 +56,9 @@ class Menu extends Component{
             <Container>
                 <Row>
                     {menu}
+                </Row>
+                <Row>
+                    {this.renderDish(this.state.selectedDish)}
                 </Row>
             </Container>
         );
