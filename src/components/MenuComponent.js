7a1faa714a5b7cfb2a432cb934b3f5d5c7a23900
yasuomaidana <yasuomaidana@gmail.com>
2022-06-20 10:46:11 -0500

    Adding DetailedMenuComponent to MenuComponent

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
+import DetailedMenuComponent from './DetailedMenuComponent';
 
 class Menu extends Component{
     
@@ -17,24 +18,6 @@ class Menu extends Component{
         this.setState({selectedDish:dish});
     }
 
-    renderDish(dish){
-        if(dish!=null){
-            return(
-            <Card>
-                <Card.Img width={100} src={dish.image} alt={dish.name} className="img-fluid rounded-start"/>
-                <Card.Body>
-                    <Card.Title>{dish.name}</Card.Title>
-                    <Card.Text>{dish.description}</Card.Text>
-                </Card.Body>
-            </Card>
-            );
-        }
-        else{
-            return(
-                <div></div>
-            );
-        }
-    }
 
     render(){
         const menu = this.props.dishes.map(dish=>{
@@ -58,7 +41,7 @@ class Menu extends Component{
                     {menu}
                 </Row>
                 <Row>
-                    {this.renderDish(this.state.selectedDish)}
+                    <DetailedMenuComponent selectedDish ={this.state.selectedDish}/>
                 </Row>
             </Container>
         );
