9bde1cde0953dc9a0a29c90fd5b84816ef4605c0
yasuomaidana <yasuomaidana@gmail.com>
2022-06-29 14:12:29 -0500

    I delegate the logic to the parent component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,48 +1,36 @@
-import React, {Component} from 'react';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card'
-import Col from 'react-bootstrap/Col'
-import DishDetail from './DishDetailComponent';
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
 
-class Menu extends Component{
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dish){
-        this.setState({selectedDish:dish});
-    }
-
-
-    render(){
-        const menu = this.props.dishes.map(dish=>{
-            return (
-                <Col key={dish.id} xs={12} md={5} className="m1 mt-4">
-                    <Card onClick={()=> this.onDishSelect(dish)}>
-                        <Card.Img width={100} src={dish.image} alt={dish.name} className="img-fluid rounded-start"/>
-
-                        <Card.ImgOverlay className='text-start ml-5'>
-                            <Card.Title><p className='h1'>{dish.name}</p></Card.Title>
-                            
-                        </Card.ImgOverlay> 
-                    </Card>
-                </Col>
-            )
-        });
-        return(
-            <Container>
-                <Row>
-                    {menu}
-                </Row>
-                <DishDetail selectedDish ={this.state.selectedDish}/>
-            </Container>
-        );
-    }
+class Menu extends Component {
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <Col
+          key={dish.id}
+          onClick={() => this.props.onClick(dish.id)}
+          xs={12} md={5}
+          className="m1 mt-4"
+          >
+          <Card>
+            <Card.Img
+              width={100}
+              src={dish.image}
+              alt={dish.name}
+              className="img-fluid rounded-start"
+            />
+            <Card.ImgOverlay className="text-start ml-5">
+              <Card.Title>
+                <p className="h1">{dish.name}</p>
+              </Card.Title>
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+      );
+    });
+    return <Row>{menu}</Row>;
+  }
 }
 
 export default Menu;
