4d4df8e17ef1680e9069889b2d30658e21888568
yasuomaidana <yasuomaidana@gmail.com>
2022-06-29 14:10:13 -0500

    Refactoring to only render main component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import './App.scss';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
-import React from 'react';
+import "./App.scss";
+import Main from "./components/MainComponent";
+import React from "react";
 
-class App extends React.Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes:DISHES
-    }
-  }
-  render () {
+class App extends React.Component {
+  render() {
     return (
-    <div>
-     <Navbar bg="primary" variant="dark">
-      <Container>
-        <Navbar.Brand href='/'><p className='h1'>Ristorante ConFusion</p></Navbar.Brand>
-      </Container>
-     </Navbar>
-     <Menu dishes={this.state.dishes}/>
-    </div>
-  )};
+      <div>
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
