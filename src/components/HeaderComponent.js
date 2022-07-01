810736309e56922b57708601cf2bcffebc77c4e8
yasuomaidana <yasuomaidana@gmail.com>
2022-07-01 17:39:36 -0500

    Modifying style to header

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,35 @@
 import { Component } from "react";
+import { Row } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 
-class Header extends Component{
-    render(){
-        return(
-            <Navbar bg="primary" variant="dark">
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <Navbar variant="dark">
           <Container>
             <Navbar.Brand href="/">
               <p className="h1">Ristorante ConFusion</p>
             </Navbar.Brand>
           </Container>
         </Navbar>
-        );
-    }
+        <Row>
+          <Col xs={6} sm={12}>
+            <div className="mt-4 p-5 text-white rounded jumbotron row-header">
+              <h1>Ristorante con Fusion</h1>
+              <p>
+                We take inspiration from the World's best cuisines, and create a
+                unique fusion experience. Our lipsmacking creations will tickle
+                your culinary senses!
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </>
+    );
+  }
 }
 
 export default Header
