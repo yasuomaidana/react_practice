95a892ebc753bac22914a4f62b01b491e8a35071
yasuomaidana <yasuomaidana@gmail.com>
2022-06-30 23:05:21 -0500

    Refactoring to functional component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,36 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 
-class Menu extends Component {
-  render() {
-    const menu = this.props.dishes.map((dish) => {
-      return (
-        <Col
-          key={dish.id}
-          onClick={() => this.props.onClick(dish.id)}
-          xs={12} md={5}
-          className="m1 mt-4"
-          >
-          <Card>
-            <Card.Img
-              width={100}
-              src={dish.image}
-              alt={dish.name}
-              className="img-fluid rounded-start"
-            />
-            <Card.ImgOverlay className="text-start ml-5">
-              <Card.Title>
-                <p className="h1">{dish.name}</p>
-              </Card.Title>
-            </Card.ImgOverlay>
-          </Card>
-        </Col>
-      );
-    });
-    return <Row>{menu}</Row>;
-  }
+const RenderDishCard = ({dish})=> {
+  return (
+    <Card>
+      <Card.Img
+        width={100}
+        src={dish.image}
+        alt={dish.name}
+        className="img-fluid rounded-start"
+      />
+      <Card.ImgOverlay className="text-start ml-5">
+        <Card.Title>
+          <p className="h1">{dish.name}</p>
+        </Card.Title>
+      </Card.ImgOverlay>
+    </Card>
+  );
 }
+
+const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
+    return (
+      <Col
+        key={dish.id}
+        onClick={() => props.onClick(dish.id)}
+        xs={12}
+        md={5}
+        className="m1 mt-4"
+      >
+        <RenderDishCard dish={dish} />
+      </Col>
+    );
+  });
+  return <Row>{menu}</Row>;
+};
 
 export default Menu;
