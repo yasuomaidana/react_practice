c97a3554ced061d054b298589dbdda4627cbdb14
yasuomaidana <yasuomaidana@gmail.com>
2022-07-04 18:13:45 -0500

    Creating and adding HomeComponent

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+const Home = (props)=>{
+    return(
+        <Container>
+            <h4>Home</h4>
+        </Container>
+    );
+}
+
+export default Home;
