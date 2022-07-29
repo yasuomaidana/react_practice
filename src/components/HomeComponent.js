dcdc4c5d5768484f66ac8ab814922ef3bcd9c93b
yasuomaidana <yasuomaidana@gmail.com>
2022-07-28 20:55:05 -0500

    Redefining home view

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,40 @@
-import { Container } from "react-bootstrap"
+import { Col, Container, Row, Card } from "react-bootstrap"
+
+
+const RenderCard = ({item})=> {
+    return (
+      <Card>
+        <Card.Img
+          width={100}
+          src={item.image}
+          alt={item.name}
+          className="img-fluid rounded-start"
+        />
+        <Card.Body className="text-start ml-5">
+          <Card.Title>
+            <p className="h1">{item.name}</p>
+          </Card.Title>
+          {item.designation?<Card.Subtitle>{item.designation}</Card.Subtitle>:null}
+          <Card.Text>{item.description}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
 
 const Home = (props)=>{
     return(
         <Container>
-            <h4>Home</h4>
+            <Row className="align-items-start">
+                <Col xs={12} md className="m-1">
+                    <RenderCard item={props.dish}/>
+                </Col>
+                <Col xs={12} md className="m-1">
+                    <RenderCard item={props.promotion}/>
+                </Col>
+                <Col xs={12} md className="m-1">
+                    <RenderCard item={props.leader}/>
+                </Col>
+            </Row>
         </Container>
     );
 }
