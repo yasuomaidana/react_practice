d1393669e81e06c8fcce1fb2c9c2eb8178cc6dd1
yasuomaidana <yasuomaidana@gmail.com>
2022-08-24 22:14:00 -0500

    Adding PostAuthor component

--- src/features/post/PostsList.js
+++ src/features/post/PostsList.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { AddPostFormComponent } from "./AddPostFormComponent";
 import { selectAllPosts } from "./postSlice";
+import PostAuthor from "./PostAuthor"
 
 const PostList = () =>{
     const posts = useSelector(selectAllPosts);
@@ -8,6 +9,7 @@ const PostList = () =>{
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0,100)}</p>
+            <PostAuthor userId={post.userId}/>
         </article>
     ));
     return(<><AddPostFormComponent/><section>{renderedPosts}</section></>)
