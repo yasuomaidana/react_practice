95e7c0b24dca499c0f4d9959911812b7d0fba97e
yasuomaidana <yasuomaidana@gmail.com>
2022-08-24 22:13:34 -0500

    Adding users to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from "../features/counter/counterSlice";
 import postsReducer from "../features/post/postSlice"
+import  usersReducer from "../features/users/usersSlice"
 export const store = configureStore({
   reducer: {
     counter:counterReducer,
-    posts:postsReducer
+    posts:postsReducer,
+    users:usersReducer
   }
 });
