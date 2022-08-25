514e40352a054eb0617028854e05171b0beeaa8d
yasuomaidana <yasuomaidana@gmail.com>
2022-08-25 18:53:17 -0500

    Ordering reducer

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -34,21 +34,29 @@ const postsSlice = createSlice({
     name:"posts",
     initialState,
     reducers:{
-        postAdded:{reducer(state,action){
-            state.push(action.payload)
-        },
-        prepare(title,content,userId){
-            return {
-                payload:{
-                id:nanoid(),
-                title,
-                content,
-                date:new Date().toISOString(),
-                userId
+        postAdded:{
+            reducer(state,action){
+                state.push(action.payload)
+            },
+            prepare(title,content,userId){ 
+                return {
+                    payload:{
+                    id:nanoid(),
+                    title,
+                    content,
+                    date:new Date().toISOString(),
+                    userId,
+                    reactions:{
+                        thumbsUp:0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                        }
                 }
             }
-        }
-    },
+            }
+        },
     }
 });
 
