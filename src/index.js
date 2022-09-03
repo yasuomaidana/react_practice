82d6e399b64b02d1ff0cfc6a7f76e49f9f0bb491
yasuomaidana <yasuomaidana@gmail.com>
2022-09-02 21:00:08 -0500

    Adding fetchUsers at begining of the program

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+import { fetchUsers } from './features/users/usersSlice';
+
+store.dispatch(fetchUsers())
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
