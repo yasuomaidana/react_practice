1342817e7eaff759e2c1ba1b3332a3288d5992ac
yasuomaidana <yasuomaidana@gmail.com>
2022-09-02 21:03:03 -0500

    Adding logic to form for posting

--- src/features/post/AddPostFormComponent.js
+++ src/features/post/AddPostFormComponent.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch,useSelector } from "react-redux";
-import { postAdded } from "./postSlice";
+import { addNewPost } from "./postSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 import React from 'react'
@@ -13,25 +13,32 @@ export const AddPostFormComponent = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const [userId, setUserId] = useState('')
-
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
+    
     const users = useSelector(selectAllUsers)
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
     const onAuthorChanged = e => setUserId(e.target.value)
 
+    const canSave = [title,content,userId].every(Boolean) && addRequestStatus === 'idle';
     const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(
-                postAdded(title,content,userId)
-            );
-            setTitle('');
-            setContent('');
-            setUserId('');
+        if (canSave) {
+            try{
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({title,body:content,userId}));
+                setTitle('');
+                setContent('');
+                setUserId('');
+            }
+            catch(err){
+                console.log(err);
+            }finally{
+                setAddRequestStatus('idle');
+            }
+
         }
     }
-
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
     const usersOptions = users.map(user => (
         <option key={user.id} value={user.id}>
