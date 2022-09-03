7f1f130bb4f75ab9a012bb62ad0df07925533d3f
yasuomaidana <yasuomaidana@gmail.com>
2022-09-02 21:01:40 -0500

    Adding get method for users

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,14 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = [
-    { id: '0', name: 'Dude Lebowski' },
-    { id: '1', name: 'Neil Young' },
-    { id: '2', name: 'Dave Gray' }
-]
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "axios";
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers',async ()=>{
+    const response = await axios.get(USERS_URL);
+    return response.data;
+});
+
+const initialState = []
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder){
+        builder.addCase(fetchUsers.fulfilled, (state,action)=>{
+            return action.payload
+        })
+    }
 });
 
 export const selectAllUsers = (state) => state.users;
