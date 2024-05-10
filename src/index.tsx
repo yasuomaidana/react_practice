edd01896e06526dba50471c10e44b834a65b0aeb
yasuomaidana <yasuomaidana@gmail.com>
2024-05-10 15:16:17 +0900

    Migrating to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 
-import { fetchUsers } from './features/users/usersSlice';
-
-store.dispatch(fetchUsers())
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
--- src/redux/selectors.js
+++
@@ -1,9 +0,0 @@
-export const getCounterState = (store) => store.counter;
-
-export const getCounterList = (store) =>
-  getCounterState(store) ? getCounterState(store).count : 0;
-
-//export const getTodoById = (store, id) =>
-//  getTodosState(store) ? { ...getTodosState(store).byIds[id], id } : {}
-
-export const getCounter = (store) => getCounterList(store);
