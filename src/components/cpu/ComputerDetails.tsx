eb9259d5363c37d51c8de330b6e7b496034dac0c
yasuomaidana <yasuomaidana@gmail.com>
2024-05-31 20:53:03 +0900

    Remove unused import in ComputerDetails component

--- src/components/cpu/ComputerDetails.tsx
+++ src/components/cpu/ComputerDetails.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import PC from "./PCModel";
 import React from "react";
 import FeatureDescription from "./FeatureDescription";
--- src/components/cpu/PCConfigurator.tsx
+++ src/components/cpu/PCConfigurator.tsx
@@ -5,7 +5,6 @@ import {
   Select,
   MenuItem,
   Button,
-  Typography,
   Modal,
   Box,
   SelectChangeEvent,
