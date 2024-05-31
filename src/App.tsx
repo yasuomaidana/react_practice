4b1d90d7a40d0b6f10b0c52f77d9beb5d23c3605
yasuomaidana <yasuomaidana@gmail.com>
2024-05-31 23:35:08 +0900

    refactor: Update App component
    This commit updates the App component to remove the ReduxExample component and add the PCConfigurator component. The ReduxExample component is commented out, and the PCConfigurator component is added to render the initial PC configuration. This change enhances the functionality and organization of the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import './App.scss';
-import ReduxExample from './components/ReduxExample';
 import NavBar from './components/navbar/NavBar';
 import { useDispatch } from 'react-redux';
 import { initializeAuht } from './hooks/auth_requests';
 import "./hooks/axiosConfig";
+import PCConfigurator from './components/cpu/PCConfigurator';
+import { initialPC } from './components/cpu/InitialPC';
 // import UserComponent from './components/UserComponent';
 // import LinearPlot from './components/LinearPlot';
+// import ReduxExample from './components/ReduxExample';
 
 function App() {
   const dispatch = useDispatch(); // Access the dispatch function
@@ -18,7 +20,8 @@ function App() {
       <NavBar />
       {/* <UserComponent /> */}
       {/* <LinearPlot /> */}
-      <ReduxExample />
+      {/* <ReduxExample /> */}
+      <PCConfigurator initialPC={initialPC}/>
     </div>
   );
 }
